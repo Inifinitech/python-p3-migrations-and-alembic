b9d3957888551c5987d6b0562fad0c6b8e745bab
Kush <kuriaisac@gmail.com>
2024-09-15 18:12:01 +0300

    updates changes

---
+++ app/migrations/versions/73f771915fba_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 73f771915fba
+Revises: 
+Create Date: 2024-09-15 17:35:07.706184
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '73f771915fba'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
